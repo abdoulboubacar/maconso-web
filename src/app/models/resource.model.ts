8c247f2675c0dcc968c092c5421d7651c4962545
Abdoul BOUBACAR <abdoul.boubacar@ineat-conseil.fr>
2017-06-07 11:21:53 +0200

    read unit

--- src/app/models/resource.model.ts
+++ src/app/models/resource.model.ts
@@ -3,13 +3,15 @@ export class ResourceModel {
   key: string;
   name: string;
   unit: string;
+  readUnit: string;
 
   public static getDeaultResource() : ResourceModel {
     let resource = new ResourceModel();
     resource.id = null;
     resource.key = null;
     resource.name = null;
-    resource.unit = null
+    resource.unit = null;
+    resource.readUnit = null;
 
     return resource;
   }
